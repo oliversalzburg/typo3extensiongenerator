a7913df7d64acd2436da450318606f19ad731189
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-03-13 19:02:59 +0100

    Bumped version

--- Typo3ExtensionGenerator/Properties/AssemblyInfo.cs
+++ Typo3ExtensionGenerator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "Typo3ExtensionGenerator" )]
-[assembly: AssemblyCopyright( "Copyright ©  2012" )]
+[assembly: AssemblyCopyright( "Copyright ©  2012-2013" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.26.1629" )]
-[assembly: AssemblyFileVersion( "1.0.26.1629" )]
+[assembly: AssemblyVersion( "1.0.27.1524" )]
+[assembly: AssemblyFileVersion( "1.0.27.1524" )]
 
 [assembly: log4net.Config.XmlConfigurator( ConfigFileExtension = "log4net", Watch = true )]
